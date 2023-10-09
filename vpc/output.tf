14e32ac0cdb0bb201f2d310d053272e45e5be226
SoundharyaVivekanandan <114381070+SoundharyaVivekanandan@users.noreply.github.com>
2023-10-09 14:17:36 +0530

    Add files via upload

---
+++ vpc/output.tf
@@ -0,0 +1,13 @@
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = aws_vpc.main.id
+}
+# vpc/outputs.tf
+output "private_subnet1_id" {
+  description = "ID of the created private subnet"
+  value       = aws_subnet.private_subnet1.id
+}
+output "private_subnet2_id" {
+  description = "ID of the created private subnet"
+  value       = aws_subnet.private_subnet2.id
+}
